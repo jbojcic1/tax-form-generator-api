b33355be95e241a0ebd901c68bbc762f16f50761
Josip Bojcic <jbojcic1@gmail.com>
2019-03-28 19:49:22 +0100

    Add business logic layer and data access layer projects.

---
+++ TaxFormGeneratorApi.Bll/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaxFormGeneratorApi.Bll
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
